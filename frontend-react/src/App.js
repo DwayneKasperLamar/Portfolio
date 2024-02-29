28a8239927a8ea25cdb55cbbd689b2656cb570fc
DriveCase <159500742+DriveCase@users.noreply.github.com>
2024-02-29 02:11:42 +0100

    mmoved

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import {About,Footer,Header,Skills,Testimonial,Work} from './Container'
+import { Navbar } from './Components/Navbar';
 
 const App = () => {
   return (
-    <h1>App</h1>
+    <div className='app'>
+      <Navbar/>
+      <Header/>
+      <About/>
+      <Work/>
+      <Skills/>
+      <Testimonial/>
+      <Footer/>
+    </div>
   )
 };
 
---
+++ frontend-react/src/Container/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Skills.scss';
+
+
+const Skills = ()=>{
+    return(
+        <div>Anout</div>
+    )
+}
+
+export default Skills
